22d5f00822b5487f78f51bd008efb04ae9ea31df
Sterett H. Mercer <sterett.mercer@ubc.ca>
2020-05-01 16:23:15 -0700

    initial commit

---
+++ R/file_utilities.R
@@ -0,0 +1,217 @@
+# This file includes functions to load model objects to generate predictions
+# and to pre-process Coh-Metrix, ReaderBench, and GAMET input files
+
+#' @title Download scoring models to the working directory.
+#' @importFrom utils download.file
+#' @export
+#' @param mod A string telling which model group to download. Options are
+#' 'rb_mod1' for ReaderBench files to generate holistic quality,
+#' 'coh_mod1' for Coh-Metrix files to generate holistic quality,
+#' and 'rb_gamet_cws1' for a merged ReaderBench and GAMET file
+#' to generate Correct Word Sequences (CWS) and Correct Minus Incorrect
+#' Word Sequences (CIWS) scores
+#' @examples
+#' #To download models to process ReaderBench files:
+#' \dontrun{
+#' download_model('rb_mod1')
+#' }
+#' #To download models to process Coh-Metrix files:
+#' \dontrun{
+#' download_model('coh_mod1')
+#' }
+#' #To download models to generate CWS/CIWS scores from ReaderBench and GAMET files:
+#' \dontrun{
+#' download_model('rb_gamet_cws1')
+#' }
+download_model <- function(mod) {
+  if (mod == 'rb_mod1') {
+    download.file(url="https://www.dropbox.com/s/yhz7uld7ifpy5na/rb_ensemble_fall7min_all.rds?dl=1",
+                  destfile="./rb_mod1a.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/jnzkhkylrps74db/rb_ensemble_win7min_all.rds?dl=1",
+                  destfile="./rb_mod1b.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/m98j8j122m5kazw/rb_ensemble_spr7min_all.rds?dl=1",
+                  destfile="./rb_mod1c.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/rreoqqip7w60vwe/rb_ensemble_fall7min_pca.rds?dl=1",
+                  destfile="./rb_mod1d.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/8ir43kfttgtv814/rb_ensemble_win7min_pca.rds?dl=1",
+                  destfile="./rb_mod1e.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/te2e9mwyywdvs5u/rb_ensemble_spr7min_pca.rds?dl=1",
+                  destfile="./rb_mod1f.rds", mode = "wb")
+  }  else if (mod == 'coh_mod1') {
+    download.file(url="https://www.dropbox.com/s/mq2nse1717ik871/coh_ensemble_fall7min_all.rds?dl=1",
+                  destfile="./coh_mod1a.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/hbocz18y8sjkb2g/coh_ensemble_win7min_all.rds?dl=1",
+                  destfile="./coh_mod1b.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/f4hn59js6ed159e/coh_ensemble_spr7min_all.rds?dl=1",
+                  destfile="./coh_mod1c.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/uk60a3zecaelwqk/coh_ensemble_fall7min_pca.rds?dl=1",
+                  destfile="./coh_mod1d.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/v3pmoptyymnr96r/coh_ensemble_win7min_pca.rds?dl=1",
+                  destfile="./coh_mod1e.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/dczd34qfvbecli9/coh_ensemble_spr7min_pca.rds?dl=1",
+                  destfile="./coh_mod1f.rds", mode = "wb")
+  } else if (mod == 'rb_gamet_cws1') {
+    download.file(url="https://www.dropbox.com/s/9qjbnp47g7uij0v/CIWS_ensemble_all7min.rds?dl=1",
+                  destfile="./CIWS_mod1a.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/u9fp961t5xsf28l/CIWS_pca_ensemble_all7min.rds?dl=1",
+                  destfile="./CIWS_mod1b.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/v88pfekdnxr3cox/CWS_ensemble_all7min.rds?dl=1",
+                  destfile="./CWS_mod1a.rds", mode = "wb")
+    download.file(url="https://www.dropbox.com/s/baipp2z1nloooid/CWS_pca_ensemble_all7min.rds?dl=1",
+                  destfile="./CWS_mod1b.rds", mode = "wb")
+  } else {
+    print("Not a valid model name")
+  }
+}
+
+#' Import a GAMET output file into R.
+#'
+#' @importFrom stats setNames
+#' @importFrom utils read.csv tail
+#' @param path A string giving the path and filename to import.
+#' @export
+#' @examples
+#' #To import as 'gamet_file' a GAMET file (sample name: gamet_output.csv)
+#' # that is stored in the working directory
+#' \dontrun{
+#' gamet_file <- import_gamet('gamet_output.csv')
+#' }
+import_gamet <- function(path) {
+  #message
+  print('Multiple instances of this error message are normal: number of items to replace is not a multiple of replacement length')
+
+  #import GAMET
+  dat1<-read.csv(path, header = T)
+  col<-as.vector(dat1$filename)
+  unit<-list()
+  for(i in 1:length(col)) {
+    ## parsing the file path into units
+    ## select the last unint in the list
+    unit[i]<-tail(unlist(strsplit(col[i], "[\\,.]")),2)
+  }
+  temp<-data.table::transpose(unit) # transpose the list to vector
+  temp<-setNames(data.frame(temp),'ID')
+  #convert into dataframe and rename it
+  dat1_merge<-cbind(temp,dat1[-1])
+  dat1_merge_r<-dplyr::mutate_all(dat1_merge, function(x) {
+    if(is.factor(x)) as.numeric(as.character(x)) else x
+  })
+  dat2 <- dat1_merge_r[,1:5]
+  dat3 <- dat2[order(dat2$ID),]
+  return(dat3)
+}
+
+#' Import a Coh-Metrix output file(.csv) into R.
+#'
+#' @importFrom stats setNames
+#' @importFrom utils read.csv tail
+#' @param path A string giving the path and filename to import.
+#' @export
+#' @examples
+#' #To import as 'coh_file' a Coh-Metrix file (sample name: coh_output.csv)
+#' #that is stored in the working directory
+#' \dontrun{
+#' coh_file <- import_coh('coh_output.csv')
+#' }
+import_coh <- function(path) {
+  dat1<-read.csv(path, header = T)
+  #TextID is field in Coh file
+  col<-as.vector(dat1$TextID)
+  unit<-list()
+  for(i in 1:length(col)) {
+    ## parsing the file path into units
+    ## select the last unint in the list
+    unit[i]<-tail(unlist(strsplit(col[i], "[\\,.]")),2)
+  }
+  temp<-data.table::transpose(unit) # transpose the list to vector
+  temp<-setNames(data.frame(temp),'ID')
+  #convert into dataframe and rename it
+  dat1_merge<-cbind(temp,dat1[-1])
+  dat1_merge_r<-dplyr::mutate_all(dat1_merge, function(x) {
+    if(is.factor(x)) as.numeric(as.character(x)) else x
+  })
+  dat2 <- dat1_merge_r[order(dat1_merge_r$ID),]
+  return(dat2)
+}
+
+#' Import a ReaderBench output file(.xlsx) into R. ReaderBench output files (.csv)
+#' should be converted to Excel format (.xlsx) before import.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom utils modifyList
+#' @export
+#' @param path A string giving the path and filename to import.
+#' @examples
+#' #To import as 'rb_file' a ReaderBench file (sample name: rb_output.xlsx)
+#' #that is stored in the working directory
+#' \dontrun{
+#' rb_file <- import_rb('rb_output.xlsx')
+#' }
+import_rb <- function(path) {
+  dat_RB<-xlsx::read.xlsx(path, header = T,1)
+  dat_RB <- dat_RB %>% dplyr::na_if("NaN")
+  asNumeric <- function(x) as.numeric(as.character(x))
+  factorsNumeric <- function(d) modifyList(d, lapply(d[, sapply(d, is.factor)],
+                                                     asNumeric))
+  dat_RB<-factorsNumeric(dat_RB)
+  dat_RB2<-dat_RB[,1:404] #exclude the sentiment analysis colums
+  names(dat_RB2)[names(dat_RB2)=="File.name"]<-"ID"
+  dat_RB3 <- dat_RB2[order(dat_RB2$ID),]
+  return(dat_RB3)
+}
+
+#' Import a ReaderBench output file(.xlsx) and GAMET output file (.csv) into R, and merge the two files.
+#' ReaderBench output files (.csv) should be converted to Excel format (.xlsx) before import.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom utils modifyList read.csv tail
+#' @importFrom stats setNames
+#' @export
+#' @param rb_path A string giving the path and ReaderBench filename to import.
+#' @param gamet_path A string giving the path and GAMET filename to import.
+#' @examples
+#' #To import as 'rb_gam_file' a ReaderBench file (sample name: rb_output.xlsx)
+#' #and GAMET file (sample name: gamet_output.csv) stored in the working
+#' #directory and then merge them
+#' \dontrun{
+#' rb_gam_file <- import_merge_gamet_rb('rb_output.xlsx', 'gamet_output.csv')
+#' }
+import_merge_gamet_rb <- function(rb_path, gamet_path) {
+  #message
+  print('Multiple instances of this error message are normal: number of items to replace is not a multiple of replacement length')
+
+  #import RB data
+  dat_RB<-xlsx::read.xlsx(rb_path, header = T,1)
+  dat_RB <- dat_RB %>% dplyr::na_if("NaN")
+  asNumeric <- function(x) as.numeric(as.character(x))
+  factorsNumeric <- function(d) modifyList(d, lapply(d[, sapply(d, is.factor)],
+                                                     asNumeric))
+  dat_RB<-factorsNumeric(dat_RB)
+  dat_RB2<-dat_RB[,1:404] #exclude the sentiment analysis colums
+  names(dat_RB2)[names(dat_RB2)=="File.name"]<-"ID"
+  dat_RB3 <- dat_RB2[order(dat_RB2$ID),]
+
+  #import GAMET data
+  datG<-read.csv(gamet_path, header = T)
+  col<-as.vector(datG$filename)
+  unit<-list()
+  for(i in 1:length(col)) {
+    ## parsing the file path into units
+    ## select the last unint in the list
+    unit[i]<-tail(unlist(strsplit(col[i], "[\\,.]")),2)
+  }
+  temp<-data.table::transpose(unit) # transpose the list to vector
+  temp<-setNames(data.frame(temp),'ID')
+  #convert into dataframe and rename it
+  datG_merge<-cbind(temp,datG[-1])
+  datG_merge_r<-dplyr::mutate_all(datG_merge, function(x) {
+    if(is.factor(x)) as.numeric(as.character(x)) else x
+  })
+  datG2 <- datG_merge_r[,1:5]
+  datG2$per_gram <- datG2$grammar/datG2$word_count
+  datG2$per_spell <- datG2$misspelling/datG2$word_count
+  datG3 <- datG2[order(datG2$ID),]
+
+  #merge RB and GAMET
+  merge(datG3, dat_RB3, by.x="ID", by.y="ID")
+}
---
+++ R/predict_values.R
@@ -0,0 +1,152 @@
+# This file includes functions to generate predicted writing quality values
+#from Readerbench and CohMetrix files. There is also a function to generate
+#writing quality composites that include GAMET spelling and grammar scores
+
+#' @title Apply scoring models for predictions
+#' @description Apply scoring models to ReaderBench, CohMetrix, and/or
+#' GAMET files. Holistic writing quality can be
+#' generated from Readerbench (model = 'rb_mod1') or
+#' Coh-Metrix files (model = 'coh_mod1'). Also,
+#' Correct Word Sequences and Correct Minus Incorrect
+#' Word Sequences can be generated from a combined
+#' Readerbench and GAMET file.
+#' @importFrom utils write.table
+#' @import caretEnsemble
+#' @param model A string telling which scoring model to use.
+#' Options are:
+#' 'rb_mod1' for ReaderBench files to generate holistic quality,
+#' 'coh_mod1' for Coh-Metrix files to generate holistic quality,
+#' and 'rb_gamet_cws1' for a merged ReaderBench and GAMET file
+#' to generate Correct Word Sequences (CWS) and Correct Minus Incorrect
+#' Word Sequences (CIWS) scores
+#' @param data The name of the R object corresponding to the data file. The
+#' import_gamet(), import_coh(), import_rb(), and import_merge_gamet_rb()
+#' functions should be used before this function
+#' to generate these data objects.
+#' @param store When store = TRUE, this function will generate scores, merge
+#' the scores into the data file, and export the file as .csv in the
+#' working directory. When store = FALSE (the default) the predicted scores
+#' are returned to a user-specified object or the R console.
+#' @param name When store = TRUE, the name parameter gives the filename for
+#' the .csv file (for example, "filename.csv") that is
+#' generated to the working directory.
+#' @export
+#' @examples
+#' #To generate holistic quality from a ReaderBench file ('rb_data')
+#' #and return scores to an object called 'rb_quality':
+#' \dontrun{
+#' rb_quality <- predict_quality('rb_mod1', rb_data, store = FALSE)
+#' }
+#' #To generate holistic quality from a Coh-Metrix file ('coh_data'), merge
+#' #predicted scores with the data file, and store as "output.csv" in the
+#' #working directory
+#' \dontrun{
+#' predict_quality('coh_mod1', coh_data, store = TRUE, name = "output.csv")
+#' }
+predict_quality <- function(model, data, store = FALSE, name = "filename.csv") {
+
+  if (model=='rb_mod1' & store == FALSE){
+    rb_mod1a <- readRDS('rb_mod1a.rds')
+    rb_mod1b <- readRDS('rb_mod1b.rds')
+    rb_mod1c <- readRDS('rb_mod1c.rds')
+    rb_mod1d <- readRDS('rb_mod1d.rds')
+    rb_mod1e <- readRDS('rb_mod1e.rds')
+    rb_mod1f <- readRDS('rb_mod1f.rds')
+
+
+    pred.1 <- predict(rb_mod1a,data)
+    pred.2 <- predict(rb_mod1b,data)
+    pred.3 <- predict(rb_mod1c,data)
+    pred.4 <- predict(rb_mod1d,data)
+    pred.5 <- predict(rb_mod1e,data)
+    pred.6 <- predict(rb_mod1f,data)
+
+    (pred.1 + pred.2 + pred.3 + pred.4 + pred.5 + pred.6)/6
+  } else if (model=='rb_mod1' & store == TRUE){
+    rb_mod1a <- readRDS('rb_mod1a.rds')
+    rb_mod1b <- readRDS('rb_mod1b.rds')
+    rb_mod1c <- readRDS('rb_mod1c.rds')
+    rb_mod1d <- readRDS('rb_mod1d.rds')
+    rb_mod1e <- readRDS('rb_mod1e.rds')
+    rb_mod1f <- readRDS('rb_mod1f.rds')
+
+
+    pred.1 <- predict(rb_mod1a,data)
+    pred.2 <- predict(rb_mod1b,data)
+    pred.3 <- predict(rb_mod1c,data)
+    pred.4 <- predict(rb_mod1d,data)
+    pred.5 <- predict(rb_mod1e,data)
+    pred.6 <- predict(rb_mod1f,data)
+
+    predicted_quality <- (pred.1 + pred.2 + pred.3 + pred.4 + pred.5 + pred.6)/6
+    data.2 <- cbind(predicted_quality,data)
+    write.table(data.2, file = name, sep = ",", row.names = FALSE)
+  } else if (model == 'coh_mod1' & store == FALSE){
+    coh_mod1a <- readRDS('coh_1a.rds')
+    coh_mod1b <- readRDS('coh_1b.rds')
+    coh_mod1c <- readRDS('coh_1c.rds')
+    coh_mod1d <- readRDS('coh_1d.rds')
+    coh_mod1e <- readRDS('coh_1e.rds')
+    coh_mod1f <- readRDS('coh_1f.rds')
+
+    pred.1 <- predict(coh_mod1a,data)
+    pred.2 <- predict(coh_mod1b,data)
+    pred.3 <- predict(coh_mod1c,data)
+    pred.4 <- predict(coh_mod1d,data)
+    pred.5 <- predict(coh_mod1e,data)
+    pred.6 <- predict(coh_mod1f,data)
+
+    (pred.1 + pred.2 + pred.3 + pred.4 + pred.5 + pred.6)/6
+  } else if (model=='coh_mod1' & store == TRUE){
+    coh_mod1a <- readRDS('coh_1a.rds')
+    coh_mod1b <- readRDS('coh_1b.rds')
+    coh_mod1c <- readRDS('coh_1c.rds')
+    coh_mod1d <- readRDS('coh_1d.rds')
+    coh_mod1e <- readRDS('coh_1e.rds')
+    coh_mod1f <- readRDS('coh_1f.rds')
+
+    pred.1 <- predict(coh_mod1a,data)
+    pred.2 <- predict(coh_mod1b,data)
+    pred.3 <- predict(coh_mod1c,data)
+    pred.4 <- predict(coh_mod1d,data)
+    pred.5 <- predict(coh_mod1e,data)
+    pred.6 <- predict(coh_mod1f,data)
+
+    predicted_quality <- (pred.1 + pred.2 + pred.3 + pred.4 + pred.5 + pred.6)/6
+    data.2 <- cbind(predicted_quality,data)
+    write.table(data.2, file = name, sep = ",", row.names = FALSE)
+  } else if (model == 'rb_gamet_cws1' & store == FALSE) {
+    CWS_mod1a <- readRDS('CWS_mod1a.rds')
+    CWS_mod1b <- readRDS('CWS_mod1b.rds')
+    CIWS_mod1a <- readRDS('CIWS_mod1a.rds')
+    CIWS_mod1b <- readRDS('CIWS_mod1b.rds')
+
+    cws.pred.1 <- predict(CWS_mod1a, data)
+    cws.pred.2 <- predict(CWS_mod1b, data)
+
+    ciws.pred.1 <- predict(CIWS_mod1a, data)
+    ciws.pred.2 <- predict(CIWS_mod1b, data)
+
+    predicted_cws <- (cws.pred.1 + cws.pred.2)/2
+    predicted_ciws <- (ciws.pred.1 + ciws.pred.2)/2
+
+    cbind(predicted_cws,predicted_ciws)
+  } else if (model == 'rb_gamet_cws1' & store == TRUE) {
+    CWS_mod1a <- readRDS('CWS_mod1a.rds')
+    CWS_mod1b <- readRDS('CWS_mod1b.rds')
+    CIWS_mod1a <- readRDS('CIWS_mod1a.rds')
+    CIWS_mod1b <- readRDS('CIWS_mod1b.rds')
+
+    cws.pred.1 <- predict(CWS_mod1a, data)
+    cws.pred.2 <- predict(CWS_mod1b, data)
+
+    ciws.pred.1 <- predict(CIWS_mod1a, data)
+    ciws.pred.2 <- predict(CIWS_mod1b, data)
+
+    predicted_cws <- (cws.pred.1 + cws.pred.2)/2
+    predicted_ciws <- (ciws.pred.1 + ciws.pred.2)/2
+
+    data.2 <- cbind(predicted_cws,predicted_ciws,data)
+    write.table(data.2, file = name, sep = ",", row.names = FALSE)
+  }
+}

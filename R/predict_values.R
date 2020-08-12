cc9c39b7dc75d35c4f952c999ac3ad94276a80d4
Sterett H. Mercer <sterett.mercer@ubc.ca>
2020-08-11 20:19:51 -0700

    finalize gamet (cws and ciws) model 1

--- R/predict_values.R
+++ R/predict_values.R
@@ -16,14 +16,12 @@
 #
 # This file includes functions to generate predicted writing quality scores
 # and written expression curriculum-based measurement scores (CWS and CIWS)
-# from Readerbench, CohMetrix, and/or GAMET files. There is also a function
-# to generate writing quality composites that include
-# GAMET spelling and grammar scores
+# from Readerbench, CohMetrix, and/or GAMET files.
 
 #declare global vars for the model objects used in predict_quality
 if(getRversion() >= "2.10")  utils::globalVariables(c("rb_mod1a", "rb_mod1b", "rb_mod1c","rb_mod1d", "rb_mod1e", "rb_mod1f",
                                                       "coh_mod1a","coh_mod1b","coh_mod1c","coh_mod1d","coh_mod1e","coh_mod1f",
-                                                      "CWS_mod1a", "CWS_mod1b", "CIWS_mod1a", "CIWS_mod1b"))
+                                                      "CWS_mod1a", "CIWS_mod1a"))
 
 #' @title Apply scoring models for predictions
 #' @description Apply scoring models to ReaderBench, CohMetrix, and/or
@@ -40,12 +38,11 @@ if(getRversion() >= "2.10")  utils::globalVariables(c("rb_mod1a", "rb_mod1b", "r
 #' Options are:
 #' 'rb_mod1' for ReaderBench files to generate holistic quality,
 #' 'coh_mod1' for Coh-Metrix files to generate holistic quality,
-#' and 'rb_gamet_cws1' for a merged ReaderBench and GAMET file
-#' to generate Correct Word Sequences (CWS) and Correct Minus Incorrect
-#' Word Sequences (CIWS) scores
+#' and 'gamet_cws1' to generate Correct Word Sequences (CWS)
+#' and Correct Minus Incorrect Word Sequences (CIWS) scores from a GAMET file.
 #' @param data The name of the R object corresponding to the data file. The
-#' \code{\link{import_gamet}}import_gamet(), \code{\link{import_coh}}import_coh(),
-#' \code{\link{import_rb}}import_rb(), and \code{\link{import_merge_gamet_rb}}import_merge_gamet_rb()
+#' \code{\link{import_gamet}}import_gamet(), \code{\link{import_coh}}import_coh(), or
+#' \code{\link{import_rb}}import_rb()
 #' functions should be used before this function
 #' to generate these data objects.
 #' @param store When store = TRUE, this function will generate scores, merge
@@ -63,7 +60,6 @@ if(getRversion() >= "2.10")  utils::globalVariables(c("rb_mod1a", "rb_mod1b", "r
 #' \code{\link{import_rb}}
 #' \code{\link{import_coh}}
 #' \code{\link{import_gamet}}
-#' \code{\link{import_merge_gamet_rb}}
 #' @examples
 #' ###Examples using sample data included in writeAlizer package
 #'
@@ -106,10 +102,7 @@ if(getRversion() >= "2.10")  utils::globalVariables(c("rb_mod1a", "rb_mod1b", "r
 #' #display quality scores
 #' coh_quality
 #'
-#' ##Example 3: Merged ReaderBench and GAMET files
-#' #see path to sample ReaderBench file
-#' file_path1
-#'
+#' ##Example 3: GAMET output file
 #' #get path of sample GAMET output file
 #' file_path3 <- system.file("extdata", "sample_gamet.csv", package = "writeAlizer")
 #'
@@ -117,14 +110,14 @@ if(getRversion() >= "2.10")  utils::globalVariables(c("rb_mod1a", "rb_mod1b", "r
 #' file_path3
 #'
 #' #import files, merge, and store as "rb_gam_file"
-#' rb_gam_file <- import_merge_gamet_rb(file_path1, file_path3)
+#' gam_file <- import_gamet(file_path3)
 #'
-#' #Generate CWS and CIWS scores from a merged ReaderBench and GAMET file
-#' #(rb_gam_file) and return scores to an object called "rb_gamet_CWS_CIWS"
-#' rb_gamet_CWS_CIWS <- predict_quality('rb_gamet_cws1', rb_gam_file, store = FALSE)
+#' #Generate CWS and CIWS scores from a GAMET file
+#' #(gam_file) and return scores to an object called "gamet_CWS_CIWS"
+#' gamet_CWS_CIWS <- predict_quality('gamet_cws1', gam_file, store = FALSE)
 #'
 #' #display quality scores
-#' rb_gamet_CWS_CIWS
+#' gamet_CWS_CIWS
 predict_quality <- function(model, data, store = FALSE, name = "filename.csv") {
   #download function
   download <- function(mod, url){
@@ -383,159 +376,60 @@ predict_quality <- function(model, data, store = FALSE, name = "filename.csv") {
 
     id_quality <- data.frame(cbind(ID=data$ID, predicted_quality))
     return(id_quality)
-  } else if (model == 'rb_gamet_cws1' & store == FALSE) {
+  } else if (model == 'gamet_cws1' & store == FALSE) {
     #check if each model object exists, if so, load it; if not, download and load it
     path_cws1a <- system.file("extdata", "CWS_mod1a.rda", package = "writeAlizer")
     if (file.exists(path_cws1a) == TRUE){
       load(system.file("extdata", "CWS_mod1a.rda", package = "writeAlizer"))
     } else {
       download("CWS_mod1a.rda",
-               "https://www.dropbox.com/s/gmqrc9keio5wnku/CWS_mod1a.rda?dl=1")
+               "https://www.dropbox.com/s/8nl11r7zrevp5x8/CWS_mod1a.rda?dl=1")
       load(system.file("extdata", "CWS_mod1a.rda", package = "writeAlizer"))
-    }
-    path_cws1b <- system.file("extdata", "CWS_mod1b.rda", package = "writeAlizer")
-    if (file.exists(path_cws1b) == TRUE){
-      load(system.file("extdata", "CWS_mod1b.rda", package = "writeAlizer"))
-    } else {
-      download("CWS_mod1b.rda",
-               "https://www.dropbox.com/s/dzfeaa6g7lh8owm/CWS_mod1b.rda?dl=1")
-      load(system.file("extdata", "CWS_mod1b.rda", package = "writeAlizer"))
     }
     path_ciws1a <- system.file("extdata", "CIWS_mod1a.rda", package = "writeAlizer")
     if (file.exists(path_ciws1a) == TRUE){
       load(system.file("extdata", "CIWS_mod1a.rda", package = "writeAlizer"))
     } else {
       download("CIWS_mod1a.rda",
-               "https://www.dropbox.com/s/b9k8jxh6pyqfs4o/CIWS_mod1a.rda?dl=1")
+               "https://www.dropbox.com/s/pcvcg4vuqowaz11/CIWS_mod1a.rda?dl=1")
       load(system.file("extdata", "CIWS_mod1a.rda", package = "writeAlizer"))
     }
-    path_ciws1b <- system.file("extdata", "CIWS_mod1b.rda", package = "writeAlizer")
-    if (file.exists(path_ciws1b) == TRUE){
-      load(system.file("extdata", "CIWS_mod1b.rda", package = "writeAlizer"))
-    } else {
-      download("CIWS_mod1b.rda",
-               "https://www.dropbox.com/s/kwb19wgpjyfw89w/CIWS_mod1b.rda?dl=1")
-      load(system.file("extdata", "CIWS_mod1b.rda", package = "writeAlizer"))
-    }
-
-    cws.pred.1 <- predict(CWS_mod1a, data)
-    cws.pred.2 <- predict(CWS_mod1b, data)
-
-    ciws.pred.1 <- predict(CIWS_mod1a, data)
-    ciws.pred.2 <- predict(CIWS_mod1b, data)
 
     predicted_tww <- data$word_count
     predicted_wsc <- data$word_count - data$misspelling
 
-    predicted_cws <- (cws.pred.1 + cws.pred.2)/2
-    predicted_ciws <- (ciws.pred.1 + ciws.pred.2)/2
+    predicted_cws <- predict(CWS_mod1a, data)
+    predicted_ciws <- predict(CIWS_mod1a, data)
 
-    return(data.frame(cbind(ID=data$ID,predicted_tww, predicted_wsc, predicted_cws,predicted_ciws)))
-  } else if (model == 'rb_gamet_cws1' & store == TRUE) {
+    return(data.frame(cbind(ID=data$ID,predicted_tww, predicted_wsc, predicted_cws, predicted_ciws)))
+  } else if (model == 'gamet_cws1' & store == TRUE) {
     #check if each model object exists, if so, load it; if not, download and load it
     path_cws1a <- system.file("extdata", "CWS_mod1a.rda", package = "writeAlizer")
     if (file.exists(path_cws1a) == TRUE){
       load(system.file("extdata", "CWS_mod1a.rda", package = "writeAlizer"))
     } else {
       download("CWS_mod1a.rda",
-               "https://www.dropbox.com/s/gmqrc9keio5wnku/CWS_mod1a.rda?dl=1")
+               "https://www.dropbox.com/s/8nl11r7zrevp5x8/CWS_mod1a.rda?dl=1")
       load(system.file("extdata", "CWS_mod1a.rda", package = "writeAlizer"))
-    }
-    path_cws1b <- system.file("extdata", "CWS_mod1b.rda", package = "writeAlizer")
-    if (file.exists(path_cws1b) == TRUE){
-      load(system.file("extdata", "CWS_mod1b.rda", package = "writeAlizer"))
-    } else {
-      download("CWS_mod1b.rda",
-               "https://www.dropbox.com/s/dzfeaa6g7lh8owm/CWS_mod1b.rda?dl=1")
-      load(system.file("extdata", "CWS_mod1b.rda", package = "writeAlizer"))
     }
     path_ciws1a <- system.file("extdata", "CIWS_mod1a.rda", package = "writeAlizer")
     if (file.exists(path_ciws1a) == TRUE){
       load(system.file("extdata", "CIWS_mod1a.rda", package = "writeAlizer"))
     } else {
       download("CIWS_mod1a.rda",
-               "https://www.dropbox.com/s/b9k8jxh6pyqfs4o/CIWS_mod1a.rda?dl=1")
+               "https://www.dropbox.com/s/pcvcg4vuqowaz11/CIWS_mod1a.rda?dl=1")
       load(system.file("extdata", "CIWS_mod1a.rda", package = "writeAlizer"))
     }
-    path_ciws1b <- system.file("extdata", "CIWS_mod1b.rda", package = "writeAlizer")
-    if (file.exists(path_ciws1b) == TRUE){
-      load(system.file("extdata", "CIWS_mod1b.rda", package = "writeAlizer"))
-    } else {
-      download("CIWS_mod1b.rda",
-               "https://www.dropbox.com/s/kwb19wgpjyfw89w/CIWS_mod1b.rda?dl=1")
-      load(system.file("extdata", "CIWS_mod1b.rda", package = "writeAlizer"))
-    }
-
-    cws.pred.1 <- predict(CWS_mod1a, data)
-    cws.pred.2 <- predict(CWS_mod1b, data)
-
-    ciws.pred.1 <- predict(CIWS_mod1a, data)
-    ciws.pred.2 <- predict(CIWS_mod1b, data)
 
     predicted_tww <- data$word_count
     predicted_wsc <- data$word_count - data$misspelling
 
-    predicted_cws <- (cws.pred.1 + cws.pred.2)/2
-    predicted_ciws <- (ciws.pred.1 + ciws.pred.2)/2
+    predicted_cws <- predict(CWS_mod1a, data)
+    predicted_ciws <- predict(CIWS_mod1a, data)
 
     data.2 <- cbind(predicted_tww, predicted_wsc,predicted_cws,predicted_ciws,data)
     write.table(data.2, file = name, sep = ",", row.names = FALSE)
 
-    return(data.frame(cbind(ID=data$ID,predicted_tww, predicted_wsc, predicted_cws,predicted_ciws)))
-  } else if (model == 'rb_gamet_cws2' & store == FALSE) {
-    #check if each model object exists, if so, load it; if not, download and load it
-    path_cws2a <- system.file("extdata", "CWS_mod2a.rda", package = "writeAlizer")
-    if (file.exists(path_cws2a) == TRUE){
-      load(system.file("extdata", "CWS_mod2a.rda", package = "writeAlizer"))
-    } else {
-      download("CWS_mod2a.rda",
-               "https://www.dropbox.com/s/agzvnd7czl0h9vl/CWS_mod2a.rda?dl=1")
-      load(system.file("extdata", "CWS_mod2a.rda", package = "writeAlizer"))
-    }
-    path_ciws2a <- system.file("extdata", "CIWS_mod2a.rda", package = "writeAlizer")
-    if (file.exists(path_ciws2a) == TRUE){
-      load(system.file("extdata", "CIWS_mod2a.rda", package = "writeAlizer"))
-    } else {
-      download("CIWS_mod2a.rda",
-               "https://www.dropbox.com/s/jaemizeh5sd945v/CIWS_mod2a.rda?dl=1")
-      load(system.file("extdata", "CIWS_mod2a.rda", package = "writeAlizer"))
-    }
-
-    predicted_tww <- data$word_count
-    predicted_wsc <- data$word_count - data$misspelling
-
-    predicted_cws <- predict(CWS_mod2a, data)
-    predicted_ciws <- predict(CIWS_mod2a, data)
-
-    return(data.frame(cbind(ID=data$ID,predicted_tww, predicted_wsc, predicted_cws,predicted_ciws)))
-  } else if (model == 'rb_gamet_cws2' & store == TRUE) {
-    #check if each model object exists, if so, load it; if not, download and load it
-    path_cws2a <- system.file("extdata", "CWS_mod2a.rda", package = "writeAlizer")
-    if (file.exists(path_cws2a) == TRUE){
-      load(system.file("extdata", "CWS_mod2a.rda", package = "writeAlizer"))
-    } else {
-      download("CWS_mod2a.rda",
-               "https://www.dropbox.com/s/agzvnd7czl0h9vl/CWS_mod2a.rda?dl=1")
-      load(system.file("extdata", "CWS_mod2a.rda", package = "writeAlizer"))
-    }
-    path_ciws2a <- system.file("extdata", "CIWS_mod2a.rda", package = "writeAlizer")
-    if (file.exists(path_ciws2a) == TRUE){
-      load(system.file("extdata", "CIWS_mod2a.rda", package = "writeAlizer"))
-    } else {
-      download("CIWS_mod2a.rda",
-               "https://www.dropbox.com/s/jaemizeh5sd945v/CIWS_mod2a.rda?dl=1")
-      load(system.file("extdata", "CIWS_mod2a.rda", package = "writeAlizer"))
-    }
-
-    predicted_tww <- data$word_count
-    predicted_wsc <- data$word_count - data$misspelling
-
-    predicted_cws <- predict(CWS_mod2a, data)
-    predicted_ciws <- predict(CIWS_mod2a, data)
-
-    data.2 <- cbind(predicted_tww, predicted_wsc,predicted_cws,predicted_ciws,data)
-    write.table(data.2, file = name, sep = ",", row.names = FALSE)
-
-    return(data.frame(cbind(ID=data$ID,predicted_tww, predicted_wsc, predicted_cws,predicted_ciws)))
+    return(data.frame(cbind(ID=data$ID,predicted_tww, predicted_wsc, predicted_cws, predicted_ciws)))
   }
 }

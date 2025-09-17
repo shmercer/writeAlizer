2d8df99a919033e9ca7d9263ddb6ee6f9a605b95
Sterett H. Mercer <sterett.mercer@ubc.ca>
2025-09-17 15:02:32 -0700

    remove unneeded test

--- tests/testthat/helper-teardown-clean.R
+++
@@ -1,7 +0,0 @@
-# tests/testthat/helper-teardown-clean.R
-testthat::teardown({
-  md <- getOption("writeAlizer.mock_dir")
-  if (is.character(md) && nzchar(md) && dir.exists(md)) {
-    unlink(md, recursive = TRUE, force = TRUE)
-  }
-})
